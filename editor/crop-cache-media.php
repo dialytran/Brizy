9df7226829a1b5a4dab07d78df1f6c8e35f9a7ac
Alex Zaharia <alecszaharia@gmail.com>
2018-07-31 11:33:22 +0300

    Fixed asset paths

--- editor/crop-cache-media.php
+++ editor/crop-cache-media.php
@@ -52,7 +52,7 @@ class Brizy_Editor_CropCacheMedia extends Brizy_Editor_Asset_StaticFile {
 		}
 
 		$external_asset_url  = $this->url_builder->external_media_url( "iW=5000&iH=any/" . $madia_name );
-		$original_asset_path = $this->url_builder->upload_path( $this->url_builder->page_asset_path( "images/" . $madia_name ) );
+		$original_asset_path = $this->url_builder->page_upload_path( "assets/images/" . $madia_name );
 
 		if ( ! file_exists( $original_asset_path ) ) {
 			// I assume that the media was already attached.
@@ -100,7 +100,7 @@ class Brizy_Editor_CropCacheMedia extends Brizy_Editor_Asset_StaticFile {
 			throw new InvalidArgumentException( "Invalid crop filter" );
 		}
 
-		$resized_page_asset_path = $this->url_builder->upload_path( $this->url_builder->page_asset_path( "images/" . $media_filter ) );
+		$resized_page_asset_path = $this->url_builder->page_upload_path( "assets/images/" . $media_filter );
 		$ext                     = pathinfo( $original_asset_path, PATHINFO_EXTENSION );
 		$resized_image_path      = $resized_page_asset_path . "/" . md5( $original_asset_path ) . '.' . $ext;
 
